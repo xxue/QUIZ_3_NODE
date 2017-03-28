aa9f7a4f126c5e81c16857bdb1532a061f788d27
Xinxin Xue <xxue@zoology.ubc.ca>
2017-03-28 01:11:58 -0700

    fixed show page, next need form submission buttons to work

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var auctions = require('./routes/auctions');
+var auctions = require("./routes/auctions");
 
 
 var app = express();
