9ebdec7ac85b49cc50b920d7bdde0db7d0b60598
Xinxin Xue <xxue@zoology.ubc.ca>
2017-03-30 15:31:56 -0700

    updated

--- routes/auctions.js
+++ routes/auctions.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const faker = require('faker');
 
-const { Auction } = require('../models/index');
+const { Auction, Bid } = require('../models/index');
 
 router.get('/new', function (req, res) {
   res.render('auctions/new');
 })
 
 router.post('/new', function (req, res, next){
-    const params = req.body;
+  const params = req.body;
     Auction
       .create({
       title: params.title,
       details: params.details,
-      // end_date: params.date,
+      end_date: new Date(params.date),
       reserve_price: params.reserveprice
       })
-      .then(
-        auctions => {
-          res.render('auctions/index', {auctions})
-        }
-      );
+      .then(auction => {res.render('auctions/show', {auction})});
 });
 
 router.get('/', function(req, res, next) {
@@ -34,18 +29,42 @@ router.get('/', function(req, res, next) {
    )
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/auctions/:id', function(req, res, next) {
   const {id} = req.params;
   Auction
     .findById(id)
-    .then(
-      auction => res.render('auctions/show', {auction})
-    )
-    .catch(
-       err => next(err)
-   )
+    .then((auction, bid) => res.render('auctions/show', {auction, bid}))
+    .catch(err => next(err))
 });
 
+router.post('/auctions/:id', function(req, res, next) {
+  const {id} = req.params;
+  const params = req.body;
+  Auction
+    .findById(id)
+    .then( auction => Bid.create({price: params.price, AuctionId: id}))
+    .then(function(){
+      if (!this.current_price || this.current_price < params.price) {
+        Auction.update({current_price: params.price}, { where: {id: id}})
+        }
+      })
+    .then((auction, bid) =>  res.render('auctions/show', {auction, bid}))
+    .catch(err => next(err))
+});
 
+router.get('/watch', function(req, res, next) {
+  Auction
+   .findAll({
+     where: {
+       watch: true
+     },
+     order: [['id', 'ASC']]
+   })
+   .then(
+     auctions => {
+       res.render('auctions/watch', {auctions})
+     }
+   );
+});
 
 module.exports = router;
